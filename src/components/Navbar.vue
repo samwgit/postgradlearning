ffb9723ab2bf6e47f5b854364b9085df0f1263bd
Samuel Wilkins <samuel.wilkins.official@gmail.com>
2022-11-23 14:26:21 -0700

    first push

---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+
+</template>
+
+<script>
+import Login from './Login.vue'
+export default {
+  setup() {
+    return {}
+  },
+  components: { Login }
+}
+</script>
+
+<style scoped>
+a:hover {
+  text-decoration: none;
+}
+
+.nav-link {
+  text-transform: uppercase;
+}
+
+.navbar-nav .router-link-exact-active {
+  border-bottom: 2px solid var(--bs-success);
+  border-bottom-left-radius: 0;
+  border-bottom-right-radius: 0;
+}
+
+@media screen and (min-width: 768px) {
+  nav {
+    height: 64px;
+  }
+}
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,49 @@
+<template>
+
+  <div class="card move" style="width: 18rem;">
+    <img src="https://thiscatdoesnotexist.com" class="card-img-top" alt="image">
+    <div class="card-body">
+      <p class="card-text">this website is a fantastic placeholder website if you are learning code.<a
+          href="https://thiscatdoesnotexist.com" title="https://thiscatdoesnotexist.com"> Here</a>
+      </p>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.move {
+  top: 5em;
+  right: -43em;
+}
+
+
+
+.home {
+  display: grid;
+  height: 80vh;
+  place-content: center;
+  text-align: center;
+  user-select: none;
+
+  .home-card {
+    width: 50vw;
+
+    >img {
+      height: 200px;
+      max-width: 200px;
+      width: 100%;
+      object-fit: contain;
+      object-position: center;
+    }
+  }
+}
+</style>
